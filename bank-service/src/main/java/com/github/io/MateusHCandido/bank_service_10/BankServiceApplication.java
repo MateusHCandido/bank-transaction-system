882c9d1dd940640786947c94e11648d33f4a75d6
Mateuzo <mateushcandido5@gmail.com>
2024-07-05 02:46:39 -0300

    first commit for jenkins test

---
+++ bank-service/src/main/java/com/github/io/MateusHCandido/bank_service_10/BankServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.io.MateusHCandido.bank_service_10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankServiceApplication.class, args);
+	}
+
+}
---
+++ bank-service/src/test/java/com/github/io/MateusHCandido/bank_service_10/BankServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.io.MateusHCandido.bank_service_10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
